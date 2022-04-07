dd123ac9be8de22b0583fba52d8ae11a2f6a4280
Cleby Francisco <clebysilva5@gmail.com>
2022-04-07 08:11:06 -0300

    CUP-13_create-footer-components

---
+++ react-typescript/src/components/atoms/footer/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss'
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <p>
+        Caker Camp - Game Vicio Redesign<br />
+        Cheesecake Labs
+      </p>
+      
+    </div>
+  )
+}
+
+export { Footer }
