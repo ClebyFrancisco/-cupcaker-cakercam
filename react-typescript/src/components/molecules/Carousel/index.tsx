e13dc69177fa1c2b0334338dc998e3d4064e822b
Cleby Francisco <clebysilva5@gmail.com>
2022-04-12 21:35:17 -0300

    CUP-8_create-carousel-component

---
+++ react-typescript/src/components/molecules/Carousel/index.tsx
@@ -0,0 +1,47 @@
+// eslint-disable-next-line
+import React from 'react'
+import { Carousel } from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { LabelType } from 'components/atoms'
+
+import styles from './styles.module.scss'
+
+interface ICarouselProps {
+  contents: {
+    title: string
+    image: string
+    type: string
+    description: string
+  }[]
+}
+const CarouselContainer = ({ contents }: ICarouselProps): JSX.Element => {
+  console.log(contents[1].description)
+  return (
+    <div className={styles.containerCarousel}>
+      <Carousel fade={true} pause={false} className={styles.carousel}>
+        {contents.map(content => {
+          return (
+            <Carousel.Item interval={5000}>
+              <img
+                className="d-block w-100"
+                src={content.image}
+                alt={content.title}
+              />
+              <div className={styles.label}>
+                <LabelType text={'#' + content.type.toUpperCase()} />
+              </div>
+              <Carousel.Caption className={styles.caption}>
+                <h3>{content.title}</h3>
+                <p>{content.description}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          )
+        })}
+      </Carousel>
+    </div>
+  )
+}
+
+export { CarouselContainer }
